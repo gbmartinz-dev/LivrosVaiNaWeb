dd9706199b7540b1a27092ec5c0398360a217008
gbmartinz-dev <gabrielpereiramartins49@gmail.com>
2024-11-27 10:32:36 -0300

    Finalizando a parte da sessão Quero Doar e estilizando

--- src/pages/queroDoar/QueroDoar.jsx
+++ src/pages/queroDoar/QueroDoar.jsx
@@ -1,5 +1,21 @@
+import iconeLivroForm from '../../assets/iconelivroform.png'
+import s from './queroDoar.module.scss'
+
 export default function QueroDoar(){
   return (
-    <h2>EU SOU O COMPONENTE QUERO DOAR</h2>
+    <section className={s.queroDoarSection}>
+      <p>Por favor, preencha o formulário com suas informações e as informações do Livro.</p>
+      <form action="">
+        <div>
+          <img src={iconeLivroForm} alt="Imagem com ícone de livro aberto com borda azul" />
+          <h2>Informações do Livro</h2>
+        </div>
+        <input type="text" name="" id="" placeholder='Título' />
+        <input type="text" name="" id="" placeholder='Categoria' />
+        <input type="text" name="" id="" placeholder='Autor' />
+        <input type="text" name="" id="" placeholder='Link da Imagem do Livro' />
+        <input type="submit" value="Doar Livro" className={s.buttonDoar} />
+      </form>
+    </section>
   )
 }
